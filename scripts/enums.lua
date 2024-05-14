c1552517e5fbbdb28c0c0a8f9c6c21555dfb38cf
MagicalMatrix <magical0matrix@gmail.com>
2024-05-14 18:27:33 +0200

    early characters implementation as new
    O also add vs screen for modified bosses

--- scripts/enums.lua
+++ scripts/enums.lua
@@ -1,8 +1,24 @@
 local Enums = {}
 
 Enums.Characters = {
-	KIMEL = 0
+	KIMEL = 0,
+	PATRYK = 2,
+	KAMIL = 8,
+	PUKASZ = 17,
+	LAWEL = 18
 }
+--DAWID
+--MAJKEL
+--MICHAL
+--KAMIL
+--ADAM
+--PAWEL
+--AGU
+--TYMEK
+--FRANEK
+--JANEK
+--WOJTEK
+--HIKOFF
 
 --it is made to easier identify wheather ludovico
 --is regular tear or brimstone or etc. within code
